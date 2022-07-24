57977a1a69318c1d67d7d7447d88acca3adef736
prerak-jain <prerakjain1007@gmail.com>
2022-07-24 11:31:50 +0530

    Added suitable titles for SEO

--- layouts/BlogLayout.js
+++ layouts/BlogLayout.js
@@ -1,29 +1,32 @@
-import { parseISO, format } from "date-fns"
+import { parseISO, format } from 'date-fns';
 
-import Container from "../components/Container"
+import Container from '../components/Container';
 export default function BlogLayout({ children, frontMatter }) {
-  return (
-    <Container
-      title={`${frontMatter.title}`}
-      description={frontMatter.summary}
-      date={new Date(frontMatter.publishedAt).toISOString()}
-      type="article"
-    >
-      <article className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16 mt-4 w-full">
-        <h1 className="font-bold text-2xl md:text-4xl tracking-tight mb-2 text-black dark:text-white">
-          {frontMatter.title}
-        </h1>
-        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
-          <div className="flex items-center">
-            <p className="text-sm text-gray-700 dark:text-gray-300 ml-1">
-              {format(parseISO(frontMatter.publishedAt), "MMMM dd, yyyy")}
-            </p>
-          </div>
-        </div>
-        <div className="prose dark:prose-dark max-w-none w-full mt-8">
-          {children}
-        </div>
-      </article>
-    </Container>
-  )
+    return (
+        <Container
+            title={`thefrontweb - ${frontMatter.title}`}
+            description={frontMatter.summary}
+            date={new Date(frontMatter.publishedAt).toISOString()}
+            type="article"
+        >
+            <article className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16 mt-4 w-full">
+                <h1 className="font-bold text-2xl md:text-4xl tracking-tight mb-2 text-black dark:text-white">
+                    {frontMatter.title}
+                </h1>
+                <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
+                    <div className="flex items-center">
+                        <p className="text-sm text-gray-700 dark:text-gray-300 ml-1">
+                            {format(
+                                parseISO(frontMatter.publishedAt),
+                                'MMMM dd, yyyy'
+                            )}
+                        </p>
+                    </div>
+                </div>
+                <div className="prose dark:prose-dark max-w-none w-full mt-8">
+                    {children}
+                </div>
+            </article>
+        </Container>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,52 @@
-import { Fragment } from 'react'
-import Head from 'next/head'
-import Container from '../components/Container'
-import BlogCard from '../components/BlogCard'
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Container from '../components/Container';
+import BlogCard from '../components/BlogCard';
 
-import { getAllPosts } from '../lib/posts-util'
+import { getAllPosts } from '../lib/posts-util';
 
 function HomePage({ posts }) {
-  const allPosts = posts
-  return (
-    <Fragment>
-      <Container>
-        <Head>
-          <title>All Posts</title>
-          <meta
-            name="description"
-            content="Posts related to frontend web development!"
-          />
-        </Head>
-        <div className="flex flex-col mb-16">
-          <h3 className="font-bold text-xl sm:text-2xl md:text-4xl tracking-tight mb-8 mt-8 text-black dark:text-white">
-            All Posts
-          </h3>
-          {allPosts.map((frontMatter) => (
-            <BlogCard key={frontMatter.title} {...frontMatter} />
-          ))}
-        </div>
-      </Container>
-    </Fragment>
-  )
+    const allPosts = posts;
+    return (
+        <Fragment>
+            <Container>
+                <Head>
+                    <title>thefrontweb - all things frontend!</title>
+                    <meta
+                        name="description"
+                        content="Posts related to frontend web development!"
+                    />
+                </Head>
+                <div className="flex flex-col mb-16">
+                    <h3 className="font-bold text-xl sm:text-2xl md:text-4xl tracking-tight mb-8 mt-8 text-black dark:text-white">
+                        All Posts
+                    </h3>
+                    {allPosts.map((frontMatter) => (
+                        <BlogCard key={frontMatter.title} {...frontMatter} />
+                    ))}
+                </div>
+            </Container>
+        </Fragment>
+    );
 }
 
 export function getStaticProps() {
-  const posts = getAllPosts('blog')
-  // console.log(posts)
+    const posts = getAllPosts('blog');
+    // console.log(posts)
 
-  posts.sort((first, second) =>
-    second.publishedAt > first.publishedAt
-      ? 1
-      : first.publishedAt > second.publishedAt
-      ? -1
-      : 0
-  )
-  // console.log(posts)
-  return {
-    props: {
-      posts,
-    },
-  }
+    posts.sort((first, second) =>
+        second.publishedAt > first.publishedAt
+            ? 1
+            : first.publishedAt > second.publishedAt
+            ? -1
+            : 0
+    );
+    // console.log(posts)
+    return {
+        props: {
+            posts,
+        },
+    };
 }
 
-export default HomePage
+export default HomePage;
