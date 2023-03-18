284114901662ca0804f5f87cf7406ba6ddcf80f1
Prerak Jain <prerakjain1007@gmail.com>
2023-03-18 10:22:01 +0530

    added analytics

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,34 +1,34 @@
-import { MDXRemote } from 'next-mdx-remote'
-import { getPostBySlug, getPostsFiles } from '../lib/posts-util'
-import BlogLayout from '../layouts/BlogLayout'
-import components from '../components/MDXComponents'
+import { MDXRemote } from "next-mdx-remote";
+import { getPostBySlug, getPostsFiles } from "../lib/posts-util";
+import BlogLayout from "../layouts/BlogLayout";
+import components from "../components/MDXComponents";
 
 export default function Blog({ source, frontMatter }) {
   return (
     <BlogLayout frontMatter={frontMatter}>
       <MDXRemote {...source} components={{ ...components }} />
     </BlogLayout>
-  )
+  );
 }
 
 export async function getStaticProps(context) {
-  const { params } = context
-  const { slug } = params
-  const data = await getPostBySlug('blog', slug)
+  const { params } = context;
+  const { slug } = params;
+  const data = await getPostBySlug("blog", slug);
   return {
     props: {
       ...data,
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const postFilenames = getPostsFiles('blog')
+  const postFilenames = getPostsFiles("blog");
 
-  const slugs = postFilenames.map((fileName) => fileName.replace(/\.mdx$/, ''))
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.mdx$/, ""));
   // console.log(slugs)
   return {
     paths: slugs.map((slug) => ({ params: { slug: slug } })),
     fallback: false,
-  }
+  };
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,18 @@
-import "../styles/globals.css"
-import { ThemeProvider } from "next-themes"
-import { MDXProvider } from "@mdx-js/react"
-import MDXComponents from "../components/MDXComponents"
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import { MDXProvider } from "@mdx-js/react";
+import MDXComponents from "../components/MDXComponents";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
         <Component {...pageProps} />
+        <Analytics />
       </MDXProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
